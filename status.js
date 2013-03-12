6ac53d73beaf2131627b2cb70298d57c9ca153d2
Nagy Ákos <nagy.akos@libreoffice.ro>
2013-03-12 22:30:05 +0200

    update js

--- status.js
+++ status.js
@@ -3,4 +3,19 @@
  * and open the template in the editor.
  */
 
+function getXMLHTTPRequest() {
+	try {
+		req = new XMLHttpRequet();
+	} catch (err) {
+					req = false;
+				}
+}
 
+function getStatus() {
+	var myurl = "getstatus.php";
+	acction = "action";
+	var modurl = myrul+"?action="+action;
+	http.open("GET", modurl, true);
+	http.onreadystatechange = useHttpResponse;
+	http.send(null);
+}
