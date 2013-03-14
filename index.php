cdfb2c3681df623c52744e4a885374719841697f
Nagy Ákos <nagy.akos@libreoffice.ro>
2013-03-14 16:38:39 +0200

    separate table

--- index.php
+++ index.php
@@ -12,17 +12,20 @@
 	// put your code here
 	?>
 	<script>
-	    function refreshList() {
-		alert('Frissul a lista');
-	    }
-
 	    function getIP(oObject) {
 		var id = oObject.id;
 		gepAdatok = {ipAddress: '10.0.0.1', hostName: 'cs54-na' };
 		alert(id);
 	    }
 
-	</script>
+            function getServerText(room) {
+                var myurl = 'table.php';
+                var modurl = myurl + "?room=" + room;
+                http.open("GET", modurl, true);
+                http.onreadystatechange = useHttpResponse;
+                http.send(null);
+            }
+        </script>
 
 	<ul>
 	<?php
@@ -37,35 +40,9 @@
 
 	<div style="float:right">
 	    <img id="spin" style="vertical-align: middle; visibility: hidden;" src="spin.gif"></img>
-	    <input id="refreshb" type="button" onclick="refreshList()" value="Refresh"></input>
+	    <input id="refreshb" type="button" onclick="getServerText('info3')" value="Refresh"></input>
 	</div>
-
-	<table id="complist" cellspacing="1">
-	    <thead>
-		<tr>
-		    <th>IP address</th><th>Computer name</th><th>Status</th>
-		</tr>
-	    </thead>
-	    <tbody>
-	    <?php
-		$action = './script.sh hostlist info3';
-		$out = shell_exec($action);
-		$hosts = explode(" ", $out);
-		while (list($key, $value) = each($hosts)) {
-		    print '<tr onclick="getIP(this)" id="'.$hosts[$key].'">';
-		    print '<td class="ip">'.$value.'</td>';
-		    print '<td class="host">'.$hosts[$key + 1].'</td>';
-		    print '<td class="stoped">Stoped</td>';
-		    print '</tr>';
-		    unset($hosts[$key + 1]);
-		}
-	    ?>
-	    </tbody>
-	    <tfoot>
-	    <th colspan="3">Finish</th>
-	    </tfoot>
-	</table>
-
-
+        
+        <div id="tableReturn"></div>
     </body>
 </html>
---
+++ table.php
@@ -0,0 +1,25 @@
+<table id="complist" cellspacing="1">
+    <thead>
+	<tr>
+	    <th>IP address</th><th>Computer name</th><th>Status</th>
+	</tr>
+    </thead>
+    <tbody>
+    <?php
+	$action = './script.sh hostlist '.$room;
+	$out = shell_exec($action);
+	$hosts = explode(" ", $out);
+	while (list($key, $value) = each($hosts)) {
+	    print '<tr onclick="getIP(this)" id="'.$hosts[$key].'">';
+	    print '<td class="ip">'.$value.'</td>';
+	    print '<td class="host">'.$hosts[$key + 1].'</td>';
+	    print '<td class="stoped">Stoped</td>';
+	    print '</tr>';
+	    unset($hosts[$key + 1]);
+	}
+    ?>
+    </tbody>
+    <tfoot>
+    <th colspan="3">Finish</th>
+    </tfoot>
+</table>
